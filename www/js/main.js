f5324b8f2c47fd1a6f86856a8584533ed62b57b1
kwokhung <chu_kwokhung@yahoo.com.hk>
2013-06-19 11:28:17 +0800

    test

--- www/js/main.js
+++ www/js/main.js
@@ -4,6 +4,14 @@ var main = function () {
         "app/util/app"
     ], function (ready, app) {
         ready(function () {
+            if (typeof cordova != "undefined") {
+                cordova.exec(function (echoValue) {
+                    alert(echoValue);
+                }, function (error) {
+                    alert(error);
+                }, "Plugin01", "echo", ["Hello"]);
+            }
+
             if (typeof device != "undefined") {
                 app.device = device;
             }
@@ -28,12 +36,6 @@ var main = function () {
                     "dojo/domReady!"
                 ], function (parser) {
                     parser.parse();
-                    var callback = function(echoValue) {
-    					alert(echoValue);
-					}
-                    cordova.exec(callback, function(err) {
-        				callback('Nothing to echo.');
-    				}, "Plugin01", "echo", ["Hello"]);
                 });
             });
         });
